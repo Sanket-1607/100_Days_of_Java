2210785f04f6e09c5f2f2279322ecd9432e108a0
Sanket K <sanketkhokade16@gmail.com>
2025-12-01 08:13:41 +0530

    Adding Variables

---
+++ 100_Days_of_Java/Variables.java
@@ -0,0 +1,21 @@
+public class Variables {
+    public static void main(String[] args) {
+        int a = 10;
+        String name = "Sanket";
+
+        System.out.println("Hello " + name);
+        System.out.println(a);
+
+        // constants
+        final float constant_value = 3.14f;
+        System.out.println(constant_value);
+
+        // comments
+
+        /*
+         * Mutiline comments
+         * one
+         */
+
+    }
+}
